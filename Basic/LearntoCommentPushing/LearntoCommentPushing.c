2487c1b7bb83cbc82afb1047e5f7212085e0fcc7
Swarup Ingale <swarupingale68@gmail.com>
2026-09-25 16:48:03 +0530

    Add C solution for Learn to CommentPushing...

---
+++ Basic/LearntoCommentPushing/LearntoCommentPushing.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int a;
+    int b;
+    int c;
+    scanf("%d %d %d", &a, &b, &c);
+
+    // Prints a
+    printf("%d\n", a);
+
+    // Prints b
+   // printf("%d\n", b);
+
+    // Prints c
+    printf("%d\n", c);
+}
